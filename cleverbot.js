7dd1bec7cab8b56dc017a93aa7387a77fec248aa
Bennett Zhang <bennett.d.zhang@gmail.com>
2020-06-04 13:03:19 -0700

    Users can speak with the bot in a voice channel

--- cleverbot.js
+++ cleverbot.js
@@ -3,99 +3,54 @@ const md5 = require("md5")
 
 let cookies
 
-module.exports = async (stimulus, context = []) => {
-    if (!cookies) {
-        const req = await superagent.get("https://www.cleverbot.com")
-        cookies = req.header["set-cookie"]
-    }
+module.exports = async (stimulus, context = [], name) => {
+  if (!cookies) {
+    const req = await superagent.get("https://www.cleverbot.com")
+    cookies = req.header["set-cookie"]
+  }
 
-    let payload = `stimulus=${sanitizeInput(stimulus)}&`
+  let payload = `stimulus=${encodeForSending(stimulus)}&`
 
-    const reverseContext = [...context].reverse()
+  for (let i = 0; i < context.length; i++)
+    payload += `vText${i + 2}=${encodeForSending(context[context.length - i - 1])}&`
+  
+  payload += "cb_settings_scripting=no&cb_settings_emotion=yes&islearning=1&icognoid=wsf&icognocheck="
+  payload += md5(payload.substring(7, 33))
 
-    for (let i = 0; i < context.length; i++) {
-        payload += `vText${i + 2}=${sanitizeInput(reverseContext[i])}&`
-    }
-    
-    payload += "cb_settings_scripting=no&cb_settings_emotion=yes&islearning=1&icognoid=wsf&icognocheck="
+  const req = await superagent.post("https://www.cleverbot.com/webservicemin?uc=UseOfficialCleverbotAPI")
+    .set("Cookie", cookies)
+    .type("text/plain")
+    .send(payload)
+  
+  let reply = decodeURIComponent(req.header["cboutput"])
 
-    payload += md5(payload.substring(7, 33))
+  if (name)
+    reply = reply.replace(/c.*?l.*?e.*?v.*?e.*?r.*?b.*?o.*?t/gi, name)
 
-    const req = await superagent.post("https://www.cleverbot.com/webservicemin?uc=UseOfficialCleverbotAPI")
-        .set("Cookie", cookies)
-        .type("text/plain")
-        .send(payload)
+  const res = {reply}
 
-    const reactEmoteMatch = req.text.match(/^{r,([^,]+),([^}]+)}{e,([^,]+),([^}]+)}(.*)$/m)
+  const reactEmoteMatch = req.text.match(/^{r,([^,]+),([^}]+)}{e,([^,]+),([^}]+)}(.*)$/m)
+  if (reactEmoteMatch) {
+    res.reaction = reactEmoteMatch[1],
+    res.reactionDegree = reactEmoteMatch[2],
+    res.emotion = reactEmoteMatch[3],
+    res.emotionDegree = reactEmoteMatch[4]
+  }
 
-    const res = {
-        reply: decodeURIComponent(req.header["cboutput"])
-    }
-
-    if (reactEmoteMatch) {
-        res.reaction = reactEmoteMatch[1],
-        res.reactionDegree = reactEmoteMatch[2],
-        res.emotion = reactEmoteMatch[3],
-        res.emotionDegree = reactEmoteMatch[4]
-    }
-
-    return res
+  return res
 }
 
-function sanitizeInput(input) {
-    let k = input.trim()
-    if (k.length < 1) {
-        return false
-    }
-    if (k === "{pass}") {
-        return encodeForSending(k)
-    }
-    const g = k.charAt(0)
-    const d = /[a-z]/i.test(g)
-    const a = /[0-9a-zA-Z\u0400-\u04FF]/.test(k.substr(0, 2))
-    const h = k.charAt(k.length - 1)
-    if (g !== g.toUpperCase()) {
-        k = g.toUpperCase() + k.substring(1)
-    }
-    if (d && ":;,-/".indexOf(h) > 0) {
-        k = k.substring(0, k.length - 1) + "."
-    } else {
-        if (a && ".?!:;,".indexOf(h) < 0) {
-            k += "."
-        }
-    }
-    let l = false
-    if (k.indexOf("\n") >= 0 || k.indexOf("\r") >= 0) {
-        l = true
-    }
-    if (/<\/?[a-z]+>|<DOCTYPE/i.test(k)) {
-        l = true
-    }
-    if (/<[^>]+>/g.test(k)) {
-        l = true
-    }
-    if (l) {
-        return false
-    }
-    return encodeForSending(k)
-}
+function encodeForSending(str) {
+  let encodedStr = ""
+  str = str.replace(/\|/g, "{*}")
 
-function encodeForSending(a) {
-    let f = ""
-    let d = ""
-    a = a.replace(/[|]/g, "{*}")
-    for (let b = 0; b <= a.length; b++) {
-        if (a.charCodeAt(b) > 255) {
-            d = escape(a.charAt(b))
-            if (d.substring(0, 2) === "%u") {
-                f += "|" + d.substring(2, d.length)
-            } else {
-                f += d
-            }
-        } else {
-            f += a.charAt(b)
-        }
-    }
-    f = f.replace("|201C", "'").replace("|201D", "'").replace("|2018", "'").replace("|2019", "'").replace("`", "'").replace("%B4", "'").replace("|FF20", "").replace("|FE6B", "")
-    return escape(f)
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 255)
+      encodedStr += escape(str[i]).replace(/^%u/, "|")
+    else
+      encodedStr += str[i]
+  }
+
+  encodedStr = encodedStr.replace("|201C", "'").replace("|201D", "'").replace("|2018", "'").replace("|2019", "'").replace("`", "'").replace("%B4", "'").replace("|FF20", "").replace("|FE6B", "")
+  return escape(encodedStr)
 }
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const fs = require("fs")
 const Discord = require("discord.js")
-const client = new Discord.Client()
+const discordClient = new Discord.Client()
 const cleverbot = require("./cleverbot.js")
+const speech = require("@google-cloud/speech")
+const speechClient = new speech.SpeechClient()
+const textToSpeech = require("@google-cloud/text-to-speech")
+const ttsClient = new textToSpeech.TextToSpeechClient()
+const {Readable} = require("stream")
 
-const messageQueue = [] // A queue containing the messages that the bot will reply to
-const chatHistory = {} // The history of messages with the bot for each user
+// Google credentials for the usage of Google's speech engines
+process.env["GOOGLE_APPLICATION_CREDENTIALS"] = "google-credentials.json"
+
+// Silence for the bot to play
+const SILENCE_FRAME = Buffer.from([0xF8, 0xFF, 0xFE])
+class Silence extends Readable {
+  _read() {
+    this.push(SILENCE_FRAME)
+  }
+}
+
+const chatHistory = {} // The history of messages each user has with the bot
+const messagesPendingCount = {} // How many pending messages there are for each channel
+const shouldQuote = {} // Whether or not to quote messages for each channel
 let emotionsToEmoji
 
 fs.readFile("params.json", "utf8", (err, data) => {
@@ -12,7 +29,7 @@ fs.readFile("params.json", "utf8", (err, data) => {
     throw err
   
   const params = JSON.parse(data)
-  client.login(params.token) // Login with the specified Discord bot token
+  discordClient.login(params.token) // Login with the specified Discord bot token
 })
 
 fs.readFile("emotions.json", "utf8", (err, data) => {
@@ -22,83 +39,178 @@ fs.readFile("emotions.json", "utf8", (err, data) => {
   emotionsToEmoji = JSON.parse(data)
 })
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+discordClient.on("ready", () => {
+  console.log(`Logged in as ${discordClient.user.tag}!`)
 })
 
-client.on("message", async msg => {
+discordClient.on("message", async msg => {
   // Make sure the message isn't from a bot
   if (msg.author.bot)
     return
 
   // If the message was sent in a guild, make sure this bot was mentioned
-  if (msg.guild && !msg.mentions.users.find(user => user.id === client.user.id))
+  if (msg.guild && !msg.mentions.users.find(user => user.id === discordClient.user.id))
     return
 
   // Remove mentions from the message content
-  msg.content = msg.content.replace(/<@.+?>|<#.+?>/g, "")
-  msg.content = msg.content.trim() // Remove excess whitespace
+  msg.content = msg.content.replace(/<@.+?>|<#.+?>/g, "").trim()
 
-  messageQueue.push(msg) // Add the message to the queue
+  // Command the bot to listen in the same voice channel as the user
+  if (msg.content.toLowerCase() === "/listen") {
+    // Make sure the command was sent in a guild
+    if (!msg.guild)
+      return
+    
+    // Check the voice channel that the user is in
+    const voiceChannel = msg.member.voice.channel
+    if (!voiceChannel)
+      return
+
+    // Join the voice channel
+    const connection = await voiceChannel.join()
+
+    // Play silence (required for the bot to receive audio)
+    connection.play(new Silence(), {type: "opus"})
+
+    const chunks = {} // The sound data that the bot is receiving from each user
+    connection.on("speaking", async (user, speaking) => {
+      // If the user has begun speaking, and no other messages are pending
+      if (speaking.has(Discord.Speaking.FLAGS.SPEAKING) && !messagesPendingCount[voiceChannel.id]) {
+        // Initialize the chunks for this user
+        if (!chunks[user.id])
+          chunks[user.id] = []
+
+        // Listen to this user and update their chunks
+        const stream = connection.receiver.createStream(user, {mode: "pcm"})
+        stream.on("data", chunk => chunks[user.id].push(chunk))
+      }
+      
+      // If the user has finished speaking
+      else if (chunks[user.id].length) {
+        // This message is pending
+        messagesPendingCount[voiceChannel.id] = ++messagesPendingCount[voiceChannel.id] || 1
+
+        // Convert the chunks to Base64
+        const audioBytes = Buffer.concat(chunks[user.id]).toString("base64")
+        chunks[user.id] = []
+
+        // Transcribe the audio data to text
+        const transcription = (await speechClient.recognize({
+          audio: {
+            content: audioBytes
+          },
+          config: {
+            encoding: "LINEAR16",
+            sampleRateHertz: 44100,
+            audioChannelCount: 2,
+            languageCode: "en-US"
+          }
+        }))[0].results
+          .map(result => result.alternatives[0].transcript)
+          .join("\n")
+
+        // This message is no longer pending
+        messagesPendingCount[voiceChannel.id]--
+
+        // Make sure something was said
+        if (!transcription)
+          return
+
+        // Get the bot's reply to the message, and play it in the voice channel
+        processMessage(new Discord.Message(discordClient, {
+          author: user,
+          content: transcription
+        }, voiceChannel))
+      }
+    })
+
+    return
+  }
+
+  // Get the bot's reply to the message, and send it to the text channel
+  processMessage(msg)
+})
+
+async function processMessage(msg) {
+  // Figure out if the channel is text or voice based
+  const isTextChannel = msg.channel.type === "dm" || msg.channel.type === "text"
+  const isVoiceChannel = msg.channel.type ===  "voice"
+
+  // This message is pending
+  messagesPendingCount[msg.channel.id] = ++messagesPendingCount[msg.channel.id] || 1
+
+  if (isTextChannel)
+    msg.channel.startTyping() // Start typing indicator
+
+  // Address the message author by their nickname, if they have one
+  const name = msg.guild ? msg.guild.member(msg.author).displayName : msg.author.username
+
+  // Get the bot's response to the message
+  const res = await cleverbot(msg.content, chatHistory[msg.author.id], name)
+  
+  let reply = ""
+
+  if (isTextChannel) {
+    // If there are multiple pending messages, quote the particular message
+    // that the bot is responding to
+    if (messagesPendingCount[msg.channel.id] > 1)
+      shouldQuote[msg.channel.id] = true
+
+    // Start the bot's reply with a block quote containing a copy of the message
+    // that it is responding to
+    if (shouldQuote[msg.channel.id])
+      reply += `> ${msg.content.replace(/\n/g, "\n> ")}\n`
+
+    // Tag the user that the bot is responding to before the reply
+    reply += `<@${msg.author.id}> ${res.reply}`
+
+    msg.channel.send(reply) // Send the bot's reply
+
+    // React to the message using the bot's emotion
+    if (emotionsToEmoji[res.emotion])
+      msg.react(emotionsToEmoji[res.emotion])    
+  } else if (isVoiceChannel) {
+    // Address the user before the reply
+    reply += `${name}, ${res.reply}`
+
+    // Join the voice channel
+    const connection = await msg.channel.join()
+
+    // Convert the bot's reply to speech
+    const audioContent = (await ttsClient.synthesizeSpeech({
+      input: {text: reply},
+      voice: {languageCode: "cmn-TW", name: "cmn-TW-Wavenet-A", ssmlGender: "FEMALE"},
+      audioConfig: {
+        audioEncoding: "MP3",
+        speakingRate: 1.15,
+        pitch: 5
+      }
+    }))[0].audioContent
+
+    // Play the speech in the voice channel
+    const readable = new Readable()
+    readable.push(audioContent)
+    readable.push(null)
+    connection.play(readable)
+  }
 
   // If this is the first time this user has messaged the bot,
   // initialize the chat history for this user
   if (!chatHistory[msg.author.id])
     chatHistory[msg.author.id] = []
-
-  // Don't process the message unless it's the only one in the queue
-  if (messageQueue.length > 1)
-    return
-
-  msg.channel.startTyping() // Start typing indicator
-
-  // Get the bot's response to the message, then process the response
-  cleverbot(msg.content, chatHistory[msg.author.id]).then(processResponse)
   
-  // Whether or not the bot should quote the message that it's responding to
-  let quoteMessage = false
+  // Add the message and response to the history
+  chatHistory[msg.author.id].push(msg.content, res.reply)
 
-  function processResponse(res) {
-    let reply = ""
+  // This message is no longer pending
+  messagesPendingCount[msg.channel.id]--
 
-    // If there's multiple messages in the queue, quote the particular message
-    // that the bot is responding to
-    if (messageQueue.length > 1)
-      quoteMessage = true
-
-    // Start the bot's reply with a block quote containing a copy of the message
-    // that it is responding to
-    if (quoteMessage)
-      reply += `> ${msg.content.replace(/\n/g, "\n> ")}\n`
-
-    // Tag the user that the bot is responding to
-    reply += `<@${msg.author.id}>, `
-
-    // Replace instances of "Cleverbot" in the response with the user's name
-    reply += res.reply.replace(/c.*?l.*?e.*?v.*?e.*?r.*?b.*?o.*?t/gi, msg.author.username)
-
-    msg.channel.send(reply) // Send the bot's reply
-
-    // Convert the bot's emotion to an emoji
-    const emoji = emotionsToEmoji[res.emotion]
-
-    // React to the message
-    if (emoji)
-      msg.react(emoji)
+  if (isTextChannel) {
+    // If all the messages in this channel have been processed,
+    // we should no longer quote messages
+    if (!messagesPendingCount[msg.channel.id])
+      shouldQuote[msg.channel.id] = false
     
-    messageQueue.shift() // Dequeue
-
-    // Add the message and response to the history
-    chatHistory[msg.author.id].push(msg.content)
-    chatHistory[msg.author.id].push(res.reply)
-
-    // If there are still more messages in the queue
-    if (messageQueue.length) {
-      msg = messageQueue[0]
-
-      // Get the bot's response to the next message in the queue, and process that response
-      cleverbot(msg.content).then(processResponse)
-    } else
-      msg.channel.stopTyping() // Stop the typing indicator
+    msg.channel.stopTyping() // Stop the typing indicator
   }
-})
+}

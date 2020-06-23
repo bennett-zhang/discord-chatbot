22b2d996f6854c4c665804bbdc77d599e9fc7695
Bennett Zhang <bennett.d.zhang@gmail.com>
2020-06-23 16:20:15 -0700

    Bot will only speak after hearing a hotword

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 A chatbot for Discord that uses artifical intelligence to reply and react to messages.
 
-You will need to create a file called params.json in the main directory containing the following:
+You will need to create a file called "params.json" in the main directory containing the following:
 
 ```
 {
-  "NAME": "Your bot's name",
-  "BOT_TOKEN": "Your Discord bot token"
+  "greetings": ["Hey", "Hi", "Hello"], // List of greetings that the bot will respond to
+  "names": ["Billy", "Jones"],         // List of names that the bot will respond to
+  "token": "..."                       // Your Discord bot token
 }
+```
+
+You will also need a file called "google-credentials.json" to use Google Cloud's speech services.
--- index.js
+++ index.js
@@ -19,16 +19,36 @@ class Silence extends Readable {
   }
 }
 
-const chatHistory = {} // The history of messages each user has with the bot
-const messagesPendingCount = {} // How many pending messages there are for each channel
+const chatHistories = {} // The history of messages each user has with the bot
+const messagesPendingCounts = {} // How many pending messages there are for each channel
 const shouldQuote = {} // Whether or not to quote messages for each channel
-let emotionsToEmoji
+const audioQueues = {} // The queue of audio that the bot is playing in each voice channel
+const hotwordWasSpoken = {} // Whether or not each user in each voice channel has spoken a hotword
+let botNames // The names that the bot will respond to
+let hotwords // The phrases that the bot will respond to
+let emotionsToEmoji // Maps possible bot emotions to their corresponding emojis
 
 fs.readFile("params.json", "utf8", (err, data) => {
   if (err)
     throw err
   
   const params = JSON.parse(data)
+
+  // List of greetings that the bot will respond to
+  // Remove non-letter, apostrophe, and space characters
+  const greetings = params.greetings.map(greeting => greeting.replace(/[^A-Za-z' ]/g, ""))
+
+  // List of names that the bot will respond to
+  // Remove non-letter, apostrophe, and space characters
+  botNames = params.names.map(name => name.replace(/[^A-Za-z' ]/g, ""))
+
+  // The hotwords are every combination of greeting + name
+  hotwords = [...botNames]
+  for (const greeting of greetings) {
+    for (const name of botNames)
+      hotwords.push(`${greeting} ${name}`)
+  }
+
   discordClient.login(params.token) // Login with the specified Discord bot token
 })
 
@@ -73,10 +93,16 @@ discordClient.on("message", async msg => {
     // Play silence (required for the bot to receive audio)
     connection.play(new Silence(), {type: "opus"})
 
+    // Keep track of the users that have said a hotword in this voice channel
+    hotwordWasSpoken[voiceChannel.id] = {}
+
+    // Initialize the audio queue for this voice channel
+    audioQueues[voiceChannel.id] = []
+
     const chunks = {} // The sound data that the bot is receiving from each user
     connection.on("speaking", async (user, speaking) => {
-      // If the user has begun speaking, and no other messages are pending
-      if (speaking.has(Discord.Speaking.FLAGS.SPEAKING) && !messagesPendingCount[voiceChannel.id]) {
+      // If the user has begun speaking
+      if (speaking.has(Discord.Speaking.FLAGS.SPEAKING)) {
         // Initialize the chunks for this user
         if (!chunks[user.id])
           chunks[user.id] = []
@@ -87,16 +113,13 @@ discordClient.on("message", async msg => {
       }
       
       // If the user has finished speaking
-      else if (chunks[user.id].length) {
-        // This message is pending
-        messagesPendingCount[voiceChannel.id] = ++messagesPendingCount[voiceChannel.id] || 1
-
+      else if (chunks[user.id] && chunks[user.id].length) {
         // Convert the chunks to Base64
         const audioBytes = Buffer.concat(chunks[user.id]).toString("base64")
         chunks[user.id] = []
 
         // Transcribe the audio data to text
-        const transcription = (await speechClient.recognize({
+        let transcription = (await speechClient.recognize({
           audio: {
             content: audioBytes
           },
@@ -104,24 +127,48 @@ discordClient.on("message", async msg => {
             encoding: "LINEAR16",
             sampleRateHertz: 44100,
             audioChannelCount: 2,
-            languageCode: "en-US"
+            languageCode: "en-US",
+            //alternativeLanguageCodes: ["vi-VN"],
+            speechContexts: [{
+              phrases: hotwords,
+              boost: 20
+            }]
           }
         }))[0].results
           .map(result => result.alternatives[0].transcript)
           .join("\n")
+        
+        // If a hotword was spoken
+        if (transcription.trim().match(new RegExp(`^(${hotwords.join("|")})$`, "i"))) {
+          // Play a sound prompting the user to speak
+          connection.play("ready.wav")
 
-        // This message is no longer pending
-        messagesPendingCount[voiceChannel.id]--
+          // Remember that this user has spoken a hotword
+          hotwordWasSpoken[voiceChannel.id][user.id] = true
+          return
+        }
+
+        // Make sure this user just spoke a hotword
+        if (!hotwordWasSpoken[voiceChannel.id][user.id])
+          return
+
+        // Remove the bot's name from the transcription
+        transcription = transcription.replace(new RegExp(botNames.join("|"), "gi"), "").trim()
 
         // Make sure something was said
         if (!transcription)
           return
+        
+        console.log(transcription)
 
+        // This user's hotword is no longer active
+        hotwordWasSpoken[voiceChannel.id][user.id] = false
+        
         // Get the bot's reply to the message, and play it in the voice channel
         processMessage(new Discord.Message(discordClient, {
           author: user,
           content: transcription
-        }, voiceChannel))
+        }, voiceChannel), connection)
       }
     })
 
@@ -132,13 +179,13 @@ discordClient.on("message", async msg => {
   processMessage(msg)
 })
 
-async function processMessage(msg) {
+async function processMessage(msg, connection) {
   // Figure out if the channel is text or voice based
   const isTextChannel = msg.channel.type === "dm" || msg.channel.type === "text"
   const isVoiceChannel = msg.channel.type ===  "voice"
 
   // This message is pending
-  messagesPendingCount[msg.channel.id] = ++messagesPendingCount[msg.channel.id] || 1
+  messagesPendingCounts[msg.channel.id] = ++messagesPendingCounts[msg.channel.id] || 1
 
   if (isTextChannel)
     msg.channel.startTyping() // Start typing indicator
@@ -147,14 +194,14 @@ async function processMessage(msg) {
   const name = msg.guild ? msg.guild.member(msg.author).displayName : msg.author.username
 
   // Get the bot's response to the message
-  const res = await cleverbot(msg.content, chatHistory[msg.author.id], name)
+  const res = await cleverbot(msg.content, chatHistories[msg.author.id], name)
   
   let reply = ""
 
   if (isTextChannel) {
     // If there are multiple pending messages, quote the particular message
     // that the bot is responding to
-    if (messagesPendingCount[msg.channel.id] > 1)
+    if (messagesPendingCounts[msg.channel.id] > 1)
       shouldQuote[msg.channel.id] = true
 
     // Start the bot's reply with a block quote containing a copy of the message
@@ -174,13 +221,11 @@ async function processMessage(msg) {
     // Address the user before the reply
     reply += `${name}, ${res.reply}`
 
-    // Join the voice channel
-    const connection = await msg.channel.join()
-
     // Convert the bot's reply to speech
     const audioContent = (await ttsClient.synthesizeSpeech({
       input: {text: reply},
       voice: {languageCode: "cmn-TW", name: "cmn-TW-Wavenet-A", ssmlGender: "FEMALE"},
+      //voice: {languageCode: "vi-VN", name: "vi-VN-Wavenet-A", ssmlGender: "FEMALE"},
       audioConfig: {
         audioEncoding: "MP3",
         speakingRate: 1.15,
@@ -188,30 +233,52 @@ async function processMessage(msg) {
       }
     }))[0].audioContent
 
-    // Play the speech in the voice channel
+    // Create a readable stream containing the audio data
     const readable = new Readable()
     readable.push(audioContent)
     readable.push(null)
-    connection.play(readable)
+
+    // Add the audio stream to the queue
+    audioQueues[msg.channel.id].push(readable)
+    
+    // If there's only one stream in the queue, play it
+    if (audioQueues[msg.channel.id].length === 1)
+      playNextStream(msg.channel, connection)
   }
 
   // If this is the first time this user has messaged the bot,
   // initialize the chat history for this user
-  if (!chatHistory[msg.author.id])
-    chatHistory[msg.author.id] = []
+  if (!chatHistories[msg.author.id])
+    chatHistories[msg.author.id] = []
   
   // Add the message and response to the history
-  chatHistory[msg.author.id].push(msg.content, res.reply)
+  chatHistories[msg.author.id].push(msg.content, res.reply)
 
   // This message is no longer pending
-  messagesPendingCount[msg.channel.id]--
+  messagesPendingCounts[msg.channel.id]--
 
   if (isTextChannel) {
     // If all the messages in this channel have been processed,
     // we should no longer quote messages
-    if (!messagesPendingCount[msg.channel.id])
+    if (!messagesPendingCounts[msg.channel.id])
       shouldQuote[msg.channel.id] = false
     
     msg.channel.stopTyping() // Stop the typing indicator
+  }
+}
+
+// Play the next stream in the queue
+async function playNextStream(voiceChannel, connection) {
+  // If there exists a stream in the queue
+  if (audioQueues[voiceChannel.id] && audioQueues[voiceChannel.id].length) {
+    // Play the first stream in the queue
+    connection.play(audioQueues[voiceChannel.id][0])
+
+    // When the stream finishes playing,
+    // remove it from the queue and play the next one
+    connection.dispatcher.on("finish", () => {
+      audioQueues[voiceChannel.id].shift()
+      playNextStream(voiceChannel, connection)
+    })
   }
 }
